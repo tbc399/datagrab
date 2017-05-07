bd518cc74ad959c325188806cfda4d5bcaee7e82
Travis Cammack <cammack@harvestd.com>
2017-05-06 19:44:12 -0500

    added grab.py and a gitignore

---
+++ grab.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+Copyright: Travis Cammack 2017
+Original Author: Travis Cammack
+Contributors:
+"""
+
+"""
+This is the driver for the datagrab system
+"""
+
+import requests
+import os
+import sys
+
+
+if __name__ == '__main__':
+    pass

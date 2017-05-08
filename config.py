2705fbc57b0403786375d32a40b80722bcba15fb
Travis Cammack <cammack@harvestd.com>
2017-05-08 06:59:10 -0500

    moved the module description into the header doc string in config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ Copyright: Travis Cammack 2017
 Original Author: Travis Cammack
 Create Date: 5/6/2017
 Contributors:
-"""
+################################################################################
 
-"""
 This module holds some sweet action settings
 """
+
 
 TRADIER_API_DOMAIN = "api.tradier.com"
 TRADIER_API_VERSION = "v1"

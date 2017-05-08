c03a72e5f676497939a878a23aba9aae233403d0
Travis Cammack <cammack@harvestd.com>
2017-05-08 17:07:39 -0500

    added price.py

---
+++ price.py
@@ -0,0 +1,20 @@
+"""
+Copyright: Travis Cammack 2017
+Original Author: Travis Cammack
+Create Date: 5/8/2017
+Contributors:
+################################################################################
+
+This module grabs standard historical prices for a given set of symbols.
+"""
+
+
+import requests
+import json
+
+def run():
+    """Entry point for price
+
+    TODO
+    """
+

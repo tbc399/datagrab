cbb7c4dcb4cf0bb79d22f2fd0dc61bd7c25ea31d
Travis Cammack <cammack@harvestd.com>
2017-05-10 20:54:09 -0500

    added opening sector file in prices.py

--- price.py
+++ price.py
@@ -11,6 +11,8 @@ This module grabs standard historical prices for a given set of symbols.
 
 import requests
 import json
+from config import *
+
 
 def run():
     """Entry point for price
@@ -18,3 +20,9 @@ def run():
     TODO
     """
 
+    with open(SECTOR_MAPPING_FILE, 'r') as f:
+        try:
+            sector_mapping = json.load(f)
+        except ValueError:
+            print "ERROR: could not load sector mapping file as JSON"
+            return False

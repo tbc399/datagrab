e0e276e0936b94d5d1ba093b2dc1ac26bdf62f25
Travis Cammack <cammack@harvestd.com>
2017-06-29 13:16:05 -0500

    added __download_symbol_price() to price.py

--- price.py
+++ price.py
@@ -11,7 +11,17 @@ This module grabs standard historical prices for a given set of symbols.
 
 import requests
 import json
+import os
 from config import *
+
+
+def __download_symbol_price(symbol, sector_dir):
+    """Download a single symbol's closing price
+
+    TODO
+    """
+
+    url = "".format()
 
 
 def run():
@@ -20,9 +30,20 @@ def run():
     TODO
     """
 
-    with open(SECTOR_MAPPING_FILE, 'r') as f:
+    sector_file = os.path.join(DATA_DOWNLOAD_DIR, SECTOR_MAPPING_FILE)
+    with open(sector_file, 'r') as f:
         try:
             sector_mapping = json.load(f)
         except ValueError:
             print "ERROR: could not load sector mapping file as JSON"
             return False
+
+    for sector_code, sector_details in sector_mapping.iteritems():
+
+        sector_dir = os.path.join(DATA_DOWNLOAD_DIR, sector_details["name"])
+        if not os.path.exists(sector_dir):
+            os.mkdir(sector_dir)
+
+        for symbol in sector_details["symbols"]:
+
+            __download_symbol_price(symbol, sector_dir)

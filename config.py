cf9ec25c3a1a63e621091cc35a1f71f7e26cc5d9
Travis Cammack <cammack@harvestd.com>
2017-06-30 17:43:58 -0500

    added skeleton for validate_configuration() in config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ Contributors:
 This module holds some sweet action settings
 """
 
+import os
 
 TRADIER_API_DOMAIN = "api.tradier.com"
 TRADIER_API_VERSION = "v1"
@@ -15,5 +16,28 @@ TRADIER_BETA_VERSION = "beta"
 TRADIER_BEARER_TOKEN = "ey39F8VMeFvhNsq4vavzeQXThcpL"
 QUERY_SYMBOL_COUNT = 20
 SECTOR_MAPPING_FILE = "sectored.json"
-DATA_DOWNLOAD_DIR = "home/travis/sonicred/TrainingData"
+DATA_DOWNLOAD_DIR = os.path.join(os.environ["HOME"], "sonicred/TrainingData")
 DATA_RANGE = 730
+
+
+def validate_configuration():
+    """Validates config variables
+    
+    TODO
+    """
+
+    #  tradier api domain
+
+    #  tradier api version
+
+    #  tradier beta version
+
+    #  tradier bearer token
+
+    #  query symbol count
+
+    #  sector mapping file
+
+    #  data download directory
+
+    #  data date range span

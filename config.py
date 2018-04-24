6cdcb425ca20056bbbfcc4905676324787f71e7c
Travis Cammack <cammack@harvestd.com>
2018-04-24 11:18:18 -0500

    changed the config.py to take in a config json file and load the glocal settings in that way

--- config.py
+++ config.py
@@ -8,22 +8,57 @@ Contributors:
 This module holds some sweet action settings
 """
 
-import os
+import json
 
-TRADIER_API_DOMAIN = "api.tradier.com"
-TRADIER_API_VERSION = "v1"
-TRADIER_BETA_VERSION = "beta"
-TRADIER_BEARER_TOKEN = "ey39F8VMeFvhNsq4vavzeQXThcpL"
-QUERY_SYMBOL_COUNT = 20
-SECTOR_MAPPING_FILE = "sectored.json"
-DATA_DOWNLOAD_DIR = os.path.join(os.environ["HOME"], "sonicred/TrainingData")
-DATA_RANGE = 500
-LAST_DATE = "2017-07-07"
+#  These are the global settings that will be populated from a config file
+TRADIER_API_DOMAIN = None
+TRADIER_API_VERSION = None
+TRADIER_BETA_VERSION = None
+TRADIER_BEARER_TOKEN = None
+QUERY_SYMBOL_COUNT = None
+START_DATE = None
+DB_NAME = None
+DB_USER = None
+DB_PASSWORD = None
+TZ = None  # EST since NASDAQ and NYSE are in New York
+
+
+def load(file_name):
+    """Load the configuration file
+
+    Load in the configuration variable values from a file
+    """
+
+    config = None
+    with open(file_name, 'r') as f:
+        config = json.load(f)
+
+    global TRADIER_API_DOMAIN
+    global TRADIER_API_VERSION
+    global TRADIER_BETA_VERSION
+    global TRADIER_BEARER_TOKEN
+    global QUERY_SYMBOL_COUNT
+    global START_DATE
+    global DB_NAME
+    global DB_USER
+    global DB_PASSWORD
+    global TZ
+
+    TRADIER_API_DOMAIN = config['tradier_api_domain']
+    TRADIER_API_VERSION = config['tradier_api_version']
+    TRADIER_BETA_VERSION = config['tradier_beta_version']
+    TRADIER_BEARER_TOKEN = config['tradier_bearer_token']
+    QUERY_SYMBOL_COUNT = config['query_symbol_count']
+    START_DATE = config['start_date']
+    DB_NAME = config['db_name']
+    DB_USER = config['db_user']
+    DB_PASSWORD = config['db_password']
+    TZ = config['tz']
 
 
 def validate_configuration():
     """Validates config variables
-    
+
     TODO
     """
 
